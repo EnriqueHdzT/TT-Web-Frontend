044e97909dff3276ba66dd34d2b209817679bc30
EnriqueHdzT <hdz_enrique@outlook.com>
2024-09-11 19:31:33 -0600

    Agrega lista de fechas consideradas inicialmente

--- app/src/components/DatesAndTerms/DatesAndTerms.tsx
+++ app/src/components/DatesAndTerms/DatesAndTerms.tsx
@@ -4,7 +4,7 @@ import "reactjs-popup/dist/index.css";
 
 export default function DatesAndTerms() {
   const [currentTerm, setCurrentTerm] = useState("Ciclo Escolar");
-  const [listOfTerms, setListOfTerms] = useState([]);
+  const [listOfTerms, setListOfTerms] = useState(["2024-1"]);
   const [isListOfTermsEmpty, setIsListOfTermsEmpty] = useState(true);
   const [latestYear, setLatestYear] = useState(0);
 
@@ -20,7 +20,7 @@ export default function DatesAndTerms() {
 
   return (
     <div className="dates-and-terms" style={{ paddingTop: "100px" }}>
-      <div className="title">
+      <div className="title-bar">
         <div className="row">
           <div className="col">
             <h1>
@@ -43,11 +43,7 @@ export default function DatesAndTerms() {
                 <ul className="dropdown-menu">
                   {listOfTerms.map((term) => (
                     <li>
-                      <a
-                        className="dropdown-item"
-                        key={term}
-                        onClick={() => updateCurrentTerm(term)}
-                      >
+                      <a className="dropdown-item" key={term} onClick={() => updateCurrentTerm(term)}>
                         {term}
                       </a>
                     </li>
@@ -56,23 +52,70 @@ export default function DatesAndTerms() {
               </div>
             )}
             <Popup
-              trigger={open => (
+              trigger={(open) => (
                 <button type="button" className="btn btn-outline-primary">
                   Nuevo Ciclo +
                 </button>
               )}
               modal
               nested
+              closeOnDocumentClick={false}
             >
-              {close => (
+              {(close) => (
                 <>
                   <h1 className="popup_title">Agregar Nuevo Ciclo</h1>
-                  <input type="number" min="1993" placeholder={latestYear < 1993 ? ("Ingrese anio") : latestYear } />
+                  <div className="input-group mb-3">
+                    <input className="form-control" type="number" min="1993" placeholder={latestYear < 1993 ? "Ingrese anio" : latestYear} />
+                    <select className="form-select">
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                    </select>
+                  </div>
+                  <div>
+                    <button className="btn btn-outline-primary" onClick={close}>
+                      Cancelar
+                    </button>
+                    <button className="btn btn-primary">Agregar</button>
+                  </div>
                 </>
               )}
             </Popup>
           </div>
         </div>
+      </div>
+      <div className="page_body">
+        <div className="fisrt_date">
+          <h2 className="first_date_text">Definir rango de fechas para subir protocolo</h2>
+          <input type="date" id="start_date" />
+          <input type="time" id="start_time" />
+          <span>al</span>
+          <input type="date" id="end_date" />
+          <input type="time" id="end_time" />
+        </div>
+        <div className="second_date">
+          <h2 className="second_date_text">Definir rango de fechas para la clasificacion de protocolos</h2>
+          <input type="date" id="start_date" />
+          <input type="time" id="start_time" />
+          <span>al</span>
+          <input type="date" id="end_date" />
+          <input type="time" id="end_time" />
+        </div>
+        <div className="third_date">
+          <h2 className="third_date_text">Definir rango de fechas para la evaluacion de protocolos</h2>
+          <input type="date" id="start_date" />
+          <input type="time" id="start_time" />
+          <span>al</span>
+          <input type="date" id="end_date" />
+          <input type="time" id="end_time" />
+        </div>
+        <div className="fourth_date">
+          <h2 className="fourth_date_text">Definir fecha de cierre de ciclo</h2>
+          <input type="date" id="closing_date" />
+          <input type="time" id="closing_time" />
+        </div>
+        <button className="btn btn-outline-danger">Cerrar Ciclo</button>
+        <button className="btn btn-outline-primary">Restablecer Datos</button>
+        <button className="btn btn-primary">Guardar</button>
       </div>
     </div>
   );
