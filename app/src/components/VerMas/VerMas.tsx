70f6f2512dabcf00ef64dca0dc01b05c5c45de9b
Jenny <jmontanor1800@alumno.ipn.mx>
2024-11-14 18:41:47 -0600

    VermasV10

--- app/src/components/VerMas/VerMas.tsx
+++ app/src/components/VerMas/VerMas.tsx
@@ -1,18 +1,28 @@
+import React, { useState } from "react";
 import "./VerMas.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 
 const VerMas = () => {
-  const [vermas] = [
-    {
-      id: 1,
-      image: ["https://i.imgur.com/GPTo4e3.png"],
-      titua: "Titulo del aviso 1",
-      info: "Objetivo. - Diseñar la correspondiente comunicación en red de una serie de estaciones de carga para vehículos eléctricos, para de esta manera disponer de la información en tiempo real para tomar correctas decisiones.",
-      tiempo: "8 minutes ago",
-    },
-    // Agrega más avisos según sea necesario
-  ];
+  const vermas = {
+    id: 1,
+    image: ["https://i.imgur.com/GPTo4e3.png", "https://i.imgur.com/GPTo4e3.png", "https://i.imgur.com/GPTo4e3.png"],
+    titua: "Titulo del aviso 1",
+    info: "Objetivo. - Diseñar la correspondiente comunicación en red de una serie de estaciones de carga para vehículos eléctricos, para de esta manera disponer de la información en tiempo real para tomar correctas decisiones.",
+    tiempo: "8 minutes ago",
+  };
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  // Función para abrir una imagen en grande
+  const openImage = (img) => {
+    setSelectedImage(img);
+  };
+
+  // Función para cerrar la imagen en grande
+  const closeImage = () => {
+    setSelectedImage(null);
+  };
 
   return (
     <div className="aviso-detalle">
@@ -31,11 +41,29 @@ const VerMas = () => {
         </div>
       </div>
 
+      {/* Miniaturas de imágenes con función de carrusel */}
       <div className="detalle-imagenes">
-        {vermas.image.map((img, index) => (
-          <img key={index} src={img} alt={`Aviso Imagen ${index + 1}`} />
-        ))}
+        <div className="thumbnail-container">
+          {vermas.image.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt={`Aviso Imagen ${index + 1}`}
+              className="thumbnail"
+              onClick={() => openImage(img)}
+            />
+          ))}
+        </div>
       </div>
+
+      {/* Imagen grande en el centro al seleccionar */}
+      {selectedImage && (
+        <div className="overlay" onClick={closeImage}>
+          <div className="large-image-container" onClick={(e) => e.stopPropagation()}>
+            <img src={selectedImage} alt="Selected" className="large-image" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
