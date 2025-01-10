d4bafe4599a0439458d1a4d6c3d771d59cb270bd
Jenny <jmontanor1800@alumno.ipn.mx>
2025-01-09 20:51:06 -0600

    viendo protocolos ultimate

--- app/src/components/VerProtocolos/components/Protocolinfo.tsx
+++ app/src/components/VerProtocolos/components/Protocolinfo.tsx
@@ -3,7 +3,11 @@ import "./Protocolinfo.scss";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEllipsisVertical,
+  faChevronDown,
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
 import "reactjs-popup/dist/index.css";
 
 export default function Protocolinfo({
@@ -18,12 +22,18 @@ export default function Protocolinfo({
 }) {
   const [userType, setUserType] = useState<string | null>(null);
   const [isOpen, setIsOpen] = useState(false);
+  const [showAlumn, setShowAlumn] = useState(false);
+  const [showDirector, setShowDirector] = useState(false);
+  const [showSinodal, setShowSinodal] = useState(false);
 
   const navigate = useNavigate();
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+  const toggleAlumn = () => setShowAlumn(!showAlumn);
+  const toggleDirector = () => setShowDirector(!showDirector);
+  const toggleSinodal = () => setShowSinodal(!showSinodal);
 
   useEffect(() => {
     const storedUserType = localStorage.getItem("userType");
@@ -50,12 +60,15 @@ export default function Protocolinfo({
   }
 
   const runSelect = async () => {
-    const response = axios.get(`http://127.0.0.1:8000/api/selectProtocol/${uuidProtocol}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
+    const response = axios.get(
+      `http://127.0.0.1:8000/api/selectProtocol/${uuidProtocol}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
     console.log(response);
     if (response.status <= 200 && response.status > 300) {
       window.location.reload();
@@ -65,12 +78,15 @@ export default function Protocolinfo({
   };
 
   const handleDelete = async () => {
-    const response = axios.delete(`http://127.0.1:8000/api/deleteProtocol/${uuidProtocol}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    });
+    const response = axios.delete(
+      `http://127.0.1:8000/api/deleteProtocol/${uuidProtocol}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
     console.log(response);
     if (response.status <= 200 && response.status > 300) {
       window.location.reload();
@@ -82,26 +98,10 @@ export default function Protocolinfo({
   return (
     <div className="p-box">
       <div className="p-tit">
-       <div className="p-tit-pro">Título de protocolo: {titleProtocol}</div>
+        <div className="p-tit-pro">Título de protocolo: {titleProtocol}</div>
         {idProtocol}
       </div>
-      <div className="cont-ee">
-        <button className="dropdown-ee" onClick={toggleDropdown}>
-          <FontAwesomeIcon icon={faEllipsisVertical} className="icon-us" />
-        </button>
-        {isOpen && (
-          <ul className="menu-ee">
-            <li
-              onClick={() => {
-                navigate(`/protocolo/${uuidProtocol}`);
-              }}
-            >
-              Editar
-            </li>
-            <li onClick={handleDelete}>Eliminar</li>
-          </ul>
-        )}
-      </div>
+
       <div className="p-buttons">
         {/* <Popup
           trigger={(open) => (
@@ -126,25 +126,43 @@ export default function Protocolinfo({
           )}
         </Popup> */}
         {buttonEnabled && statusProtocol == "validating" && (
-          <button type="button" className="btn btn-outline-primary" onClick={goToValidate}>
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={goToValidate}
+          >
             Validar
           </button>
         )}
         {buttonEnabled && statusProtocol == "classifying" && (
-          <button type="button" className="btn btn-outline-primary" onClick={goToClasify}>
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={goToClasify}
+          >
             Clasificar
           </button>
         )}
         {buttonEnabled && statusProtocol == "selecting" && (
-          <button type="button" className="btn btn-outline-primary" onClick={() => runSelect()}>
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={() => runSelect()}
+          >
             Seleccionar
           </button>
         )}
-        {buttonEnabled && (statusProtocol == "evaluatingFirst" || statusProtocol == "evaluatingSecond") && (
-          <button type="button" className="btn btn-outline-primary" onClick={() => seeEvaluar()}>
-            Evaluar
-          </button>
-        )}
+        {buttonEnabled &&
+          (statusProtocol == "evaluatingFirst" ||
+            statusProtocol == "evaluatingSecond") && (
+            <button
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={() => seeEvaluar()}
+            >
+              Evaluar
+            </button>
+          )}
         {buttonEnabled && statusProtocol == "correcting" && (
           <button
             type="button"
@@ -154,38 +172,105 @@ export default function Protocolinfo({
             Corregir Protocolo
           </button>
         )}
-        <button type="button" className="btn btn-outline-primary" onClick={seeDocument}>
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={seeDocument}
+        >
           Documento
         </button>
-        <button type="button" className="btn btn-outline-primary" onClick={seeStatus}>
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={seeStatus}
+        >
           Ver Status
         </button>
       </div>
+      <div className="cont-ee">
+        <button className="dropdown-ee" onClick={toggleDropdown}>
+          <FontAwesomeIcon icon={faEllipsisVertical} className="icon-us" />
+        </button>
+        {isOpen && (
+          <ul className="menu-ee">
+            <li
+              onClick={() => {
+                navigate(`/protocolo/${uuidProtocol}`);
+              }}
+            >
+              Editar
+            </li>
+            <li onClick={handleDelete}>Eliminar</li>
+          </ul>
+        )}
+      </div>
       <div className="p-info">
-          {studentList.map((student) => (
-            <div className="p-alumn">
+        {/* Sección Alumnos */}
+        <div className="section-header" onClick={toggleAlumn}>
+          <span>Alumnos</span>
+          <FontAwesomeIcon icon={showAlumn ? faChevronUp : faChevronDown} />
+        </div>
+        {showAlumn && (
+          <div className="p-alumn">
+            {studentList.map((student) => (
               <a key={student.curriculum}>
-                {student.name + " " + student.curriculum}
-                <br></br>
+                <span className="p-subtitle">Nombre:</span>
+                {student.name +
+                  " " +
+                  student.lastname +
+                  " " +
+                  (student.second_lastname
+                    ? " " + student.second_lastname
+                    : "") +
+                  " "}
+                  <span className="p-subtitle">Carrera:</span>{student.career}
+                <br />
               </a>
-            </div>
-          ))}
-          {directorList.map((director) => (
-            <div className="p-director">
+            ))}
+          </div>
+        )}
+
+        {/* Sección Director */}
+        <div className="section-header" onClick={toggleDirector}>
+          <span>Director(es)</span>
+          <FontAwesomeIcon icon={showDirector ? faChevronUp : faChevronDown} />
+        </div>
+        {showDirector && (
+          <div className="p-director">
+            {directorList.map((director) => (
               <a key={director.precedence}>
-                {director.name + " " + director.precedence}
-                <br></br>
+                <span className="p-subtitle">Nombre:</span>
+                {director.name + " " +
+                  director.lastname +
+                  " " +
+                  (director.second_lastname
+                    ? " " + director.second_lastname
+                    : "") + " "}
+                <span className="p-subtitle">Procedencia:</span>{director.precedence}
+                <br />
               </a>
-            </div>
-          ))}
-          {sinodalList.map((sinodal, i) => (
-            <div className="p-sinodal">
-              <a key={i}>
-                {sinodal.name + " " + sinodal.academies.join(", ")}
-                <br></br>
-              </a>
-            </div>
-          ))}
+            ))}
+          </div>
+        )}
+
+        {/* Sección Sinodal */}
+        <div className="section-header" onClick={toggleSinodal}>
+  <span>Sinodales</span>
+  <FontAwesomeIcon icon={showSinodal ? faChevronUp : faChevronDown} />
+</div>
+{showSinodal && (
+  <div className="p-sinodal">
+    {sinodalList.map((sinodal, i) => (
+      <a key={i}>
+        <span className="p-subtitle">Nombre:</span>
+        {sinodal.name + " " + sinodal.lastname + " " + (sinodal.second_lastname ? " " + sinodal.second_lastname : "")}
+        <span className="p-subtitle">Academias:</span>{sinodal.academies.join(", ")}
+        <br />
+      </a>
+    ))}
+  </div>
+)}
+
       </div>
     </div>
   );
